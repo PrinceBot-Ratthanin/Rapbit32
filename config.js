b22e47f9d9d9a96075c51f408733f41100636fb7
PrinceBot-Ratthanin <50706633+PrinceBot-Ratthanin@users.noreply.github.com>
2020-07-09 02:04:20 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: "Rapbit32",
+  platform: "arduino-esp32",
+  title: "Rapbit32",
+  description: "หุ่นยนต์แห่งการเรียนรู้",
+  author: "Ratthanin Kittisriphong",
+  website: "https://PrinceBot.net",
+  email: "print081@gmail.com",
+  git: "",
+  image: "/static/Rapbit32.png",
+  version: "0.1.0",
+};
