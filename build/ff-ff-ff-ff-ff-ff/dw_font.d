36701d3830abfd0e69d38529dcd2a69e298b98d1
PrinceBot-Ratthanin <50706633+PrinceBot-Ratthanin@users.noreply.github.com>
2020-07-09 02:09:34 +0700

    Add files via upload

---
+++ build/ff-ff-ff-ff-ff-ff/dw_font.d
@@ -0,0 +1,8 @@
+C:\Users\OMEN\Desktop\KB-IDE_Portable_v1.2.0_win64\KB-IDE\boards\Rapbit32\build\ff-ff-ff-ff-ff-ff\dw_font.o: \
+ C:\Users\OMEN\Desktop\KB-IDE_Portable_v1.2.0_win64\KB-IDE\boards\Rapbit32\include\dw_font.c \
+ C:\Users\OMEN\Desktop\KB-IDE_Portable_v1.2.0_win64\KB-IDE\boards\Rapbit32\include\dw_font.h \
+ C:\Users\OMEN\Desktop\KB-IDE_Portable_v1.2.0_win64\KB-IDE\platforms\arduino-esp32\sdk\include\newlib/stdint.h \
+ C:\Users\OMEN\Desktop\KB-IDE_Portable_v1.2.0_win64\KB-IDE\platforms\arduino-esp32\sdk\include\newlib/machine/_default_types.h \
+ C:\Users\OMEN\Desktop\KB-IDE_Portable_v1.2.0_win64\KB-IDE\platforms\arduino-esp32\sdk\include\newlib/sys/features.h \
+ C:\Users\OMEN\Desktop\KB-IDE_Portable_v1.2.0_win64\KB-IDE\platforms\arduino-esp32\sdk\include\newlib/sys/_intsup.h \
+ C:\Users\OMEN\Desktop\KB-IDE_Portable_v1.2.0_win64\KB-IDE\platforms\arduino-esp32\lib\include/pgmspace.h
---
+++ build/ff-ff-ff-ff-ff-ff/irPronto.d
@@ -0,0 +1,2 @@
+C:\Users\OMEN\Desktop\KB-IDE_Portable_v1.2.0_win64\KB-IDE\boards\Rapbit32\build\ff-ff-ff-ff-ff-ff\irPronto.o: \
+ C:\Users\OMEN\Desktop\KB-IDE_Portable_v1.2.0_win64\KB-IDE\boards\Rapbit32\include\irremote\irPronto.cpp
