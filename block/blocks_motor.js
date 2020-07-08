e72ad5aebf439d744f5bf53b35b9c4f200d05055
PrinceBot-Ratthanin <50706633+PrinceBot-Ratthanin@users.noreply.github.com>
2020-07-09 02:00:16 +0700

    Add files via upload

---
+++ block/blocks_motor.js
@@ -0,0 +1,256 @@
+
+//Block from IKB1 
+
+module.exports = function(Blockly){
+  'use strict';
+
+Blockly.Blocks['Rapbit32_motor'] = {
+  init: function() {
+    this.appendDummyInput()
+      .appendField("set motor")
+      .appendField(new Blockly.FieldDropdown([["Left","1"], ["Right","2"]]), "ch")
+      .appendField("direction")
+      .appendField(new Blockly.FieldDropdown([["Forward","1"], ["Backward", "2"]]), "dir");
+    this.appendValueInput("speed")
+      .setCheck("Number")
+      .appendField("speed");
+    this.appendDummyInput()
+      .appendField("%");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(180);
+    this.setTooltip("");
+  }
+};
+
+Blockly.Blocks['Rapbit32_servo'] = {
+  init: function() {
+    this.appendDummyInput()
+      .appendField("set servo")
+      .appendField(new Blockly.FieldDropdown([["1","1"], ["2","2"]]), "ch");
+    this.appendValueInput("angle")
+      .setCheck("Number")
+      .appendField("degree");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip("");
+  }
+};
+
+Blockly.Blocks['Rapbit32_servo2'] = {
+  init: function() {
+    this.appendDummyInput()
+      .appendField("set servo")
+      .appendField(new Blockly.FieldDropdown([["1","1"], ["2","2"]]), "ch")
+      .appendField("direction")
+      .appendField(new Blockly.FieldDropdown([["Forward","1"], ["Backward", "2"]]), "dir");
+    this.appendValueInput("speed")
+      .setCheck("Number")
+      .appendField("speed");
+    this.appendDummyInput()
+      .appendField("%");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip("");
+  }
+};
+Blockly.Blocks['Rapbit32_motor_forward'] = {
+  init: function() {
+    this.appendValueInput("speed")
+      .setCheck("Number")
+      .appendField("Move Forward at speed");
+    this.appendDummyInput()
+      .appendField("%");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(180);
+    this.setTooltip("");
+  }
+};
+Blockly.Blocks['Rapbit32_motor_backward'] = {
+  init: function() {
+    this.appendValueInput("speed")
+      .setCheck("Number")
+      .appendField("Move Backward at speed");
+    this.appendDummyInput()
+      .appendField("%");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(180);
+    this.setTooltip("");
+  }
+};
+Blockly.Blocks['Rapbit32_motor_forward2'] = {
+  init: function() {
+    this.appendValueInput("speed1")
+      .setCheck("Number")
+      .appendField("Move Forward left wheel at speed");
+    this.appendDummyInput()
+      .appendField("%");
+      this.appendValueInput("speed2")
+      .setCheck("Number")
+      .appendField("and right wheel at speed");
+    this.appendDummyInput()
+      .appendField("%");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(180);
+    this.setTooltip("");
+  }
+};
+Blockly.Blocks['Rapbit32_motor_backward2'] = {
+  init: function() {
+    this.appendValueInput("speed1")
+      .setCheck("Number")
+      .appendField("Move Backward left wheel at speed");
+    this.appendDummyInput()
+      .appendField("%");
+      this.appendValueInput("speed2")
+      .setCheck("Number")
+      .appendField("and right wheel at speed");
+    this.appendDummyInput()
+      .appendField("%");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(180);
+    this.setTooltip("");
+  }
+};
+Blockly.Blocks['Rapbit32_motor_spin'] = {
+  init: function() {
+    this.appendDummyInput()
+      .appendField("Motor Turn :")
+      .appendField(new Blockly.FieldDropdown([["Left","1"], ["Right","2"]]), "direction");
+    this.appendValueInput("speed")
+      .setCheck("Number")
+      .appendField("speed");
+    this.appendDummyInput()
+      .appendField("%");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(180);
+    this.setTooltip("");
+  }
+};
+Blockly.Blocks['Rapbit32_motor_turn'] = {
+  init: function() {
+    this.appendDummyInput()
+      .appendField("Motor Spin :")
+      .appendField(new Blockly.FieldDropdown([["Left","1"], ["Right","2"]]), "direction");
+    this.appendValueInput("speed")
+      .setCheck("Number")
+      .appendField("speed");
+    this.appendDummyInput()
+      .appendField("%");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(180);
+    this.setTooltip("");
+  }
+};
+Blockly.Blocks['Rapbit32_motor_turn_left'] = {
+  init: function() {
+    this.appendValueInput("speed")
+      .setCheck("Number")
+      .appendField("Turn Left at speed");
+    this.appendDummyInput()
+      .appendField("%");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(180);
+    this.setTooltip("");
+  }
+};
+Blockly.Blocks['Rapbit32_motor_turn_right'] = {
+  init: function() {
+    this.appendValueInput("speed")
+      .setCheck("Number")
+      .appendField("Turn Right at speed");
+    this.appendDummyInput()
+      .appendField("%");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(180);
+    this.setTooltip("");
+  }
+};
+Blockly.Blocks['Rapbit32_motor_spin_left'] = {
+  init: function() {
+    this.appendValueInput("speed")
+      .setCheck("Number")
+      .appendField("Spin Left at speed");
+    this.appendDummyInput()
+      .appendField("%");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(180);
+    this.setTooltip("");
+  }
+};
+Blockly.Blocks['Rapbit32_motor_spin_right'] = {
+  init: function() {
+    this.appendValueInput("speed")
+      .setCheck("Number")
+      .appendField("Spin Right at speed");
+    this.appendDummyInput()
+      .appendField("%");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(180);
+    this.setTooltip("");
+  }
+};
+Blockly.Blocks['Rapbit32_motor_stop'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Stop Moving");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(180);
+    this.setTooltip("");
+  }
+};
+Blockly.Blocks['Rapbit32_motor_stop_ch'] = {
+  init: function() {
+    this.appendDummyInput()
+      .appendField("Stop Moving Ch")
+      .appendField(new Blockly.FieldDropdown([["ALL","0"],["1","1"], ["2","2"]]), "ch")
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(180);
+    this.setTooltip("");
+  }
+};
+
+Blockly.Blocks['Run_following_of_line'] = {
+  init: function() {
+    this.appendValueInput("speed")
+      .setCheck("Number")
+      .appendField("speed for line following");
+    this.appendDummyInput()
+      .appendField("%");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(330);
+    this.setTooltip("");
+  }
+};
+}
