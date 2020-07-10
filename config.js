455c42df1a73ce6c769c73593e2882e09592babb
PrinceBot-Ratthanin <50706633+PrinceBot-Ratthanin@users.noreply.github.com>
2020-07-11 05:17:53 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
   author: "Ratthanin Kittisriphong",
   website: "https://PrinceBot.net",
   email: "print081@gmail.com",
-  git: "https://github.com/PrinceBot-Ratthanin/Rapbit32/",
+  git: "https://github.com/PrinceBot-Ratthanin/Rapbit32",
   image: "/static/Rapbit32.png",
-  version: "0.1.0",
+  version: "1.0.0",
 };
